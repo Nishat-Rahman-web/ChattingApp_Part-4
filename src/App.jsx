6d53fea29875bda64ef1246b9591f487c3655637
Nishat Rahman <168478182+Nishat-Rahman-web@users.noreply.github.com>
2024-08-19 20:01:44 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import './App.css'
+import LoginPage from './Pages/LoginPage'
+import NotFound from './Components/Login/NotFound/NotFound'
+import { ToastContainer } from 'react-toastify'
+import RegisterPage from './Pages/RegisterPage'
+import app from './firebase.config'
+
+function App() {
+  const route = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Route>
+    )
+  )
+  return (
+    <>
+      <ToastContainer />
+      <RouterProvider router={route}/>
+    </>
+  )
+}
+
+export default App
